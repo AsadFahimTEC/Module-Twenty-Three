92fcc2fbd157ea79213c1e6ddc699c403c1772d8
AsadFahimTEC <fah485434@gmail.com>
2025-12-27 09:23:50 +0600

    implementing create post api part 2 done

--- Prisma Blog App/src/modules/post/post.controller.ts
+++ Prisma Blog App/src/modules/post/post.controller.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from "express";
+import { postService } from "./post.service";
+
 
 const createPost = async(req:Request, res:Response) => {
-    // res.send("Create a new post");
-    console.log({req, res});
+    try {
+        const result = await postService.createPost(req.body)
+        res.status(201).json(result)
+    } catch (e) {
+        res.status(400).json({
+            error: "Post creation fail",
+            details: e
+        })
+    }
 }
 
 export const PostController = {
