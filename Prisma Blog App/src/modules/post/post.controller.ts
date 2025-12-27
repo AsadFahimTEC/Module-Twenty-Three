90d4d8e4955af802da6411d66e6bb5697fed8c0f
AsadFahimTEC <fah485434@gmail.com>
2025-12-27 09:04:48 +0600

    implement create post api part 1

---
+++ Prisma Blog App/src/modules/post/post.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+const createPost = async(req:Request, res:Response) => {
+    // res.send("Create a new post");
+    console.log({req, res});
+}
+
+export const PostController = {
+    createPost
+}
